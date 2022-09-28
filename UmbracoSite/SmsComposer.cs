4b62eda91c34bf0d8dcd3356e99c1a818140011f
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-09-28 19:28:44 +0000

    Add SmsWorkflow

---
+++ UmbracoSite/SmsComposer.cs
@@ -0,0 +1,16 @@
+using Twilio.AspNet.Core;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Forms.Core.Providers;
+
+namespace UmbracoSite;
+
+public class SmsComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Services.AddTwilioClient();
+        builder.WithCollectionBuilder<WorkflowCollectionBuilder>()
+            .Add<SmsWorkflow>();
+    }
+}
